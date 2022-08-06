639a36a33922468d9a1a6e9110b4b6decc91860a
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2022-08-06 21:13:37 +0200

    Improve updating, reduce log spam

--- custom_components/ytmdesktop_remote/media_player.py
+++ custom_components/ytmdesktop_remote/media_player.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
+import asyncio
 from datetime import timedelta
-from typing import List, Optional, Type
+from typing import Optional
 
 import aioytmdesktopapi
+import logging
 
 from homeassistant.components.media_player import (
     MediaPlayerEntity,
     MediaPlayerEntityFeature,
-    MediaPlayerDeviceClass,
 )
 from homeassistant.components.media_player.const import (
     MEDIA_TYPE_MUSIC,
@@ -21,7 +22,9 @@ from homeassistant.const import (
     STATE_PLAYING,
     STATE_PAUSED,
 )
+from homeassistant.core import HomeAssistant
 from homeassistant.util.dt import utcnow
+
 
 from .const import DOMAIN, LOGGER
 
@@ -36,10 +39,14 @@ SUPPORTED_MEDIAPLAYER_COMMANDS = (
     # | MediaPlayerEntityFeature.SEEK     API command not working, see https://github.com/ytmdesktop/ytmdesktop/issues/885
 )
 
-SCAN_INTERVAL = timedelta(seconds=1)
+# Using timedelta of 5 seconds often gives Server Disconnect exceptions
+# maybe YTMD has a 5 second timeout that conflicts occasionally?
+SCAN_INTERVAL = timedelta(seconds=6)
 
 
-async def async_setup_entry(hass, config_entry: ConfigEntry, async_add_entities):
+async def async_setup_entry(
+    hass: HomeAssistant, config_entry: ConfigEntry, async_add_entities
+):
 
     api = hass.data[DOMAIN][config_entry.entry_id]
     async_add_entities([YtmDesktopMediaPlayer(config_entry.entry_id, api)], True)
@@ -52,15 +59,23 @@ class YtmDesktopMediaPlayer(MediaPlayerEntity):
     _attr_media_image_remotely_accessible = True
     _attr_name = "YouTube Music Desktop"  # API does not expose a name. Pick a decent default, user can change
 
-    def __init__(self, device_id: str, api: aioytmdesktopapi.YtmDesktop) -> None:
+    def schedule_ha_update(func):
+        async def _decorator(self: YtmDesktopMediaPlayer, *args, **kwargs):
+            await func(self, *args, **kwargs)
+            await asyncio.sleep(1)  # When immediately updating sometimes it is too soon
+            self.async_schedule_update_ha_state(True)
+
+        return _decorator
+
+    def __init__(self, configentry_id: str, api: aioytmdesktopapi.YtmDesktop) -> None:
         self._api = api
         self._available = False
         self._position = None
         self._position_updated_at = None
 
-        self._attr_unique_id = device_id
+        self._attr_unique_id = configentry_id
         self._attr_device_info = {
-            "identifiers": {(DOMAIN, device_id)},
+            "identifiers": {(DOMAIN, configentry_id)},
         }
 
     async def async_update(self) -> None:
@@ -76,7 +91,11 @@ class YtmDesktopMediaPlayer(MediaPlayerEntity):
                 self._position = self._api.player.seekbar_current_position
                 self._position_updated_at = utcnow()
         except aioytmdesktopapi.RequestError:
-            LOGGER.debug("ConnectionError for %s", self._api.host)
+            if self._available:
+                LOGGER.error("Error updating %s", self._api.host)
+            if LOGGER.level == logging.DEBUG:
+                LOGGER.exception("Exception during update for %s" % self._api.host)
+
             self._available = False
         except aioytmdesktopapi.Unauthorized as err:
             LOGGER.error("Authentication error for %s", self._api.host)
@@ -107,33 +126,40 @@ class YtmDesktopMediaPlayer(MediaPlayerEntity):
         """Flag of media commands that are supported."""
         return SUPPORTED_MEDIAPLAYER_COMMANDS
 
+    @schedule_ha_update
     async def async_set_volume_level(self, volume) -> None:
         """Set volume level, convert range from 0..1."""
         await self._api.send_command.player_set_volume(volume * 100)
 
+    @schedule_ha_update
     async def async_volume_up(self) -> None:
         """Volume up media player."""
         await self._api.send_command.player_volume_up()
 
+    @schedule_ha_update
     async def async_volume_down(self) -> None:
         """Volume down media player."""
         await self._api.send_command.player_volume_down()
 
+    @schedule_ha_update
     async def async_media_play(self) -> None:
         await self._api.send_command.track_play()
 
+    @schedule_ha_update
     async def async_media_pause(self) -> None:
         await self._api.send_command.track_pause()
 
+    @schedule_ha_update
     async def async_media_next_track(self) -> None:
         await self._api.send_command.track_next()
 
+    @schedule_ha_update
     async def async_media_previous_track(self) -> None:
         await self._api.send_command.track_previous()
 
+    @schedule_ha_update
     async def async_media_seek(self, position) -> None:
         if self._api.track:
-            LOGGER.warning("position: %d", position)
             await self._api.send_command.player_set_seekbar(int(position))
 
     @property
@@ -147,6 +173,7 @@ class YtmDesktopMediaPlayer(MediaPlayerEntity):
             return REPEAT_MODE_OFF
         return None
 
+    @schedule_ha_update
     async def async_set_repeat(self, repeat) -> None:
         """Set repeat mode."""
         if repeat == REPEAT_MODE_ALL:
